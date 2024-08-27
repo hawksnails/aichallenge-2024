c59870109c083dde70816134117b197a0dc19ec5
Michi-Tsubaki <tsubaki@g.ecc.u-tokyo.ac.jp>
2024-08-27 21:37:39 +0900

    avoidance_param_change / anti-wall crash!

--- aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/include/simple_pure_pursuit/simple_pure_pursuit.hpp
+++ aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/include/simple_pure_pursuit/simple_pure_pursuit.hpp
@@ -1,9 +1,11 @@
 #ifndef SIMPLE_PURE_PURSUIT_HPP_
 #define SIMPLE_PURE_PURSUIT_HPP_
 #include <autoware_auto_control_msgs/msg/ackermann_control_command.hpp>
+#include <autoware_auto_vehicle_msgs/msg/gear_command.hpp>
 #include <autoware_auto_planning_msgs/msg/trajectory.hpp>
 #include <autoware_auto_planning_msgs/msg/trajectory_point.hpp>
 #include <autoware_auto_vehicle_msgs/msg/steering_report.hpp>
+#include <autoware_auto_vehicle_msgs/msg/velocity_report.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
@@ -12,9 +14,11 @@
 #include <rclcpp/rclcpp.hpp>
 namespace simple_pure_pursuit {
 using autoware_auto_control_msgs::msg::AckermannControlCommand;
+using autoware_auto_vehicle_msgs::msg::GearCommand;
 using autoware_auto_planning_msgs::msg::Trajectory;
 using autoware_auto_planning_msgs::msg::TrajectoryPoint;
 using autoware_auto_vehicle_msgs::msg::SteeringReport;
+using autoware_auto_vehicle_msgs::msg::VelocityReport;
 using geometry_msgs::msg::Pose;
 using geometry_msgs::msg::Twist;
 using nav_msgs::msg::Odometry;
@@ -27,8 +31,10 @@ class SimplePurePursuit : public rclcpp::Node {
   rclcpp::Subscription<Trajectory>::SharedPtr sub_trajectory_;
   rclcpp::Subscription<Float64MultiArray>::SharedPtr sub_objects_;
   rclcpp::Subscription<SteeringReport>::SharedPtr sub_steering_;
+  rclcpp::Subscription<VelocityReport>::SharedPtr sub_velocity_;
   // publishers
   rclcpp::Publisher<AckermannControlCommand>::SharedPtr pub_cmd_;
+  rclcpp::Publisher<GearCommand>::SharedPtr pub_gear_;
   // timer
   rclcpp::TimerBase::SharedPtr timer_;
   // updated by subscribers
@@ -37,6 +43,7 @@ class SimplePurePursuit : public rclcpp::Node {
   Float64MultiArray::SharedPtr objects_;
 
   double current_steering_;
+  double current_velocity_;
 
   // pure pursuit parameters
   const double wheel_base_;
@@ -45,6 +52,8 @@ class SimplePurePursuit : public rclcpp::Node {
   const double speed_proportional_gain_;
   const bool use_external_target_vel_;
   const double external_target_vel_;
+  
+
  private:
   void onTimer();
   bool subscribeMessageAvailable();
--- aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
+++ aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
@@ -26,6 +26,8 @@ SimplePurePursuit::SimplePurePursuit()
   external_target_vel_(declare_parameter<float>("external_target_vel", 0.0))
 {
   pub_cmd_ = create_publisher<AckermannControlCommand>("output/control_cmd", 1);
+  pub_gear_ = create_publisher<GearCommand>("output/gear_cmd", 1);
+  
   
   sub_kinematics_ = create_subscription<Odometry>(
     "input/kinematics", 1, [this](const Odometry::SharedPtr msg) { odometry_ = msg; });
@@ -36,6 +38,9 @@ SimplePurePursuit::SimplePurePursuit()
   sub_steering_ = create_subscription<SteeringReport>(
     "input/steering", 10,
     [this](const SteeringReport::SharedPtr msg) { current_steering_ = msg->steering_tire_angle; });
+  sub_velocity_ = create_subscription<VelocityReport>(
+    "input/velocity", 10,
+    [this](const VelocityReport::SharedPtr msg) { current_velocity_ = msg->longitudinal_velocity; });
   
   using namespace std::literals::chrono_literals;
   timer_ =
@@ -54,6 +59,14 @@ AckermannControlCommand zeroAckermannControlCommand(rclcpp::Time stamp)
   return cmd;
 }
 
+GearCommand zeroGearCommand(rclcpp::Time stamp)
+{
+  GearCommand gear_cmd;
+  gear_cmd.stamp = stamp;
+  gear_cmd.command = 2;
+  return gear_cmd;
+}
+
 void SimplePurePursuit::onTimer()
 {
   // check data
@@ -65,13 +78,15 @@ void SimplePurePursuit::onTimer()
   
   // publish zero command
   AckermannControlCommand cmd = zeroAckermannControlCommand(get_clock()->now());
+  GearCommand gear_cmd = zeroGearCommand(get_clock()->now());
   
-  if (closet_traj_point_idx == trajectory_->points.size() - 1 ||
-      trajectory_->points.size() <= 5) {
+  if (
+    (closet_traj_point_idx == trajectory_->points.size() - 1) ||
+    (trajectory_->points.size() <= 5)) {
     cmd.longitudinal.speed = 0.0;
     cmd.longitudinal.acceleration = -10.0;
-    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "Reached the goal");
-  } else {
+    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "reached to the goal");
+   }else {
     // get closest trajectory point from current position
     TrajectoryPoint closet_traj_point = trajectory_->points.at(closet_traj_point_idx);
     
@@ -115,30 +130,53 @@ void SimplePurePursuit::onTimer()
       double object_distance = std::hypot(object_x - odometry_->pose.pose.position.x, object_y - odometry_->pose.pose.position.y);
       double object_angle = std::atan2(object_y - odometry_->pose.pose.position.y, object_x - odometry_->pose.pose.position.x);
 
-      double object_radius_sum = object_radius + 4.2;
+      double object_radius_sum = object_radius + 4.5;
       //車体から見た物体の角度
       double object_angle_diff = object_angle - tf2::getYaw(odometry_->pose.pose.orientation);
       
-      if (current_steering_ < 1.0){
+      if (current_steering_ < 1.0 && current_steering_ > -1.0){
       //障害物が近づいたとき回避する
       if (object_distance < object_radius_sum && object_distance > object_radius+0.4){
         //物体が前方にあるとき
-        if ((object_angle_diff <0.5&&  object_angle_diff > 0.2) ||  (object_angle_diff < -0.2 && object_angle_diff > -0.5)){
+        if ((object_angle_diff <0.5&&  object_angle_diff > 0.1) ||  (object_angle_diff < -0.1 && object_angle_diff > -0.5)){
           //物体が右側にあるとき
           if (object_angle_diff < 0){
             //左側に避ける
-            cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) + 0.36;
+            if (current_velocity_ > 7){
+              cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) + 0.4;
+            } else {
+              cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) + 0.6;
+            }
           } else {
             //右側に避ける
-            cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) - 0.36;
+            if (current_velocity_ > 7){
+              cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) - 0.4;
+            } else {
+              cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) - 0.6;
+            }
           }
         }
       }
+      }
+
+      //速度が0のとき，壁にぶつかっているので，ステアを切る
+      if (current_velocity_ < 0.3){
+        //現在の座標を見る
+        double current_x = odometry_->pose.pose.position.x;
+        double current_y = odometry_->pose.pose.position.y;
+        gear_cmd.command = 20;
+        //今の位置から1m後ろに戻る
+        while (std::hypot(current_x - odometry_->pose.pose.position.x, current_y - odometry_->pose.pose.position.y) < 2.0){
+          cmd.longitudinal.acceleration = -3.0;
+        }
+        //通常の動作に戻す
+        gear_cmd.command = 2;
       }
       std::cout << "Object x: " << object_x << " Object y: " << object_y << std::endl;
     }
   }
   pub_cmd_->publish(cmd);
+  pub_gear_->publish(gear_cmd);
 }
 
 bool SimplePurePursuit::subscribeMessageAvailable()
