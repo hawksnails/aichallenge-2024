4e7e724722ea7469de7664973f7e26c145ba2dba
22tachibana <tachibana-takatoki357@g.ecc.u-tokyo.ac.jp>
2024-10-10 12:25:34 +0900

    出た

--- aichallenge/workspace/src/aichallenge_submit/path_to_trajectory/src/path_to_trajectory.cpp
+++ aichallenge/workspace/src/aichallenge_submit/path_to_trajectory/src/path_to_trajectory.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <cmath>
 
+namespace my_namespace {
+
 constexpr double kmh = 1000.0/3600.0;
 
 struct PathPoint {
@@ -35,13 +37,17 @@ public:
     static constexpr double max_acc = 2.0; // m/s^2
     
     Line(std::vector<std::string> data) : Particle() {
-        this->st.x = std::stod(data[1]);
-        this->st.y = std::stod(data[2]);
-        this->start_vel = std::stod(data[3]) * kmh;
-        this->ed.x = std::stod(data[4]);
-        this->ed.y = std::stod(data[5]);
-        this->end_vel = std::stod(data[6]) * kmh;
+      try {
+        this->st.x = std::stod(data.at(1));
+        this->st.y = std::stod(data.at(2));
+        this->start_vel = std::stod(data.at(3)) * kmh;
+        this->ed.x = std::stod(data.at(4));
+        this->ed.y = std::stod(data.at(5));
+        this->end_vel = std::stod(data.at(6)) * kmh;
         this->dir = std::atan2(this->ed.y - this->st.y, this->ed.x - this->st.x);
+      } catch (const std::exception& e) {
+        std::cout << "in constructor of Line:" << e.what() << std::endl;
+      }
     }
 
     XY st, ed;
@@ -77,12 +83,16 @@ public:
     double vel;
 
     Arc(std::vector<std::string> data) : Particle() {
-        this->center.x = std::stod(data[1]);
-        this->center.y = std::stod(data[2]);
-        this->radius = std::stod(data[3]);
-        this->start_angle = std::stod(data[4]);
-        this->end_angle = std::stod(data[5]);
-        this->vel = std::stod(data[6]) * kmh;
+      try {
+        this->center.x = std::stod(data.at(1));
+        this->center.y = std::stod(data.at(2));
+        this->radius = std::stod(data.at(3));
+        this->start_angle = std::stod(data.at(4));
+        this->end_angle = std::stod(data.at(5));
+        this->vel = std::stod(data.at(6)) * kmh;
+      } catch (const std::exception& e) {
+        std::cout << "in constructor of Arc:" << e.what() << std::endl;
+      }
     };
 
     std::vector<PathPoint> generate_path(double dt, double st=0) override {
@@ -108,40 +118,52 @@ public:
 class PathTrajectory {
 public:
     std::vector<std::unique_ptr<Particle>> particles;
-    double path_time;
+    double path_time=0;
 
     PathTrajectory(std::string filepath) {
-        std::ifstream file(filepath);
-        std::string line;
-        while (std::getline(file, line)) {
-            std::stringstream ss(line);
-            std::vector<std::string> data;
-            std::string token;
-            while (std::getline(ss, token, ',')) {
-                data.push_back(token);
+        try {
+            std::ifstream file(filepath);
+            std::string line;
+            while (std::getline(file, line)) {
+                std::cout << "line:" << line << std::endl;
+                std::stringstream ss(line);
+                std::vector<std::string> data;
+                std::string token;
+                while (std::getline(ss, token, ',')) {
+                    data.push_back(token);
+                }
+                if (line.at(0) == 'L') {
+                    particles.push_back(std::make_unique<Line>(data));
+                } else if (line.at(0) == 'A') {
+                    particles.push_back(std::make_unique<Arc>(data));
+                }
             }
-            if (line[0] == 'L') {
-                particles.push_back(std::make_unique<Line>(data));
-            } else if (line[0] == 'A') {
-                particles.push_back(std::make_unique<Arc>(data));
-            }
+        } catch (const std::exception& e) {
+            std::cout << "in constructor of PathTrajectory" << e.what() << std::endl;
         }
+        std::cout << "size of particles = " << particles.size() << std::endl;
     }
 
     std::vector<PathPoint> generate_path(double dt) {
-        std::vector<PathPoint> path = particles[0]->generate_path(dt);
-        double time_offset = particles[0]->path_time;
-        for (int i = 1; i < particles.size(); i++) {
-            double st = dt - (time_offset - path.back().time);
-            auto next_path = particles[i]->generate_path(dt, st);
-            for (int j = 0; j < next_path.size(); j++) {
-                next_path[j].time += time_offset;
-                path.push_back(next_path[j]);
-            }
-            time_offset += particles[i]->path_time;
+        try {
+          std::vector<PathPoint> path = particles.at(0)->generate_path(dt);
+          double time_offset = particles.at(0)->path_time;
+          for (int i = 1; i < particles.size(); i++) {
+              double st = dt - (time_offset - path.back().time);
+              auto next_path = particles.at(i)->generate_path(dt, st);
+              for (int j = 0; j < next_path.size(); j++) {
+                  next_path.at(j).time += time_offset;
+                  path.push_back(next_path.at(j));
+              }
+              time_offset += particles.at(i)->path_time;
+          }
+          path_time = time_offset;
+          return path;
+
+        } catch (const std::exception& e) {
+            std::cout << "in generate_path of PathTrajectory" << e.what() << std::endl;
+            return {};
         }
-        path_time = time_offset;
-        return path;
     }
 
     void write_path(std::string filepath, double dt = 0.01) {
@@ -149,28 +171,54 @@ public:
         std::ofstream outfile(filepath);
         outfile << "time,x,y,th,vel,curvature,acc" << std::endl;
         for (int i = 0; i < path.size(); i++) {
-            outfile << path[i].time << "," << path[i].x << "," << path[i].y << "," << path[i].th << "," << path[i].vel << "," << path[i].curvature << "," << path[i].acc << std::endl;
+            outfile << path.at(i).time << "," << path.at(i).x << "," << path.at(i).y << "," << path.at(i).th << "," << path.at(i).vel << "," << path.at(i).curvature << "," << path.at(i).acc << std::endl;
         }
     }
 };
+
+
+}  // namespace my_namespace
 
 PathToTrajectory::PathToTrajectory() : Node("path_to_trajectory_node") {
     using std::placeholders::_1;
 
     pub_ = this->create_publisher<autoware_auto_planning_msgs::msg::Trajectory>("output", 1);
+    // sub_ = this->create_subscription<autoware_auto_planning_msgs::msg::PathWithLaneId>("input", 1, std::bind(&PathToTrajectory::callback, this, _1));
     sub_ = this->create_subscription<autoware_auto_planning_msgs::msg::PathWithLaneId>("input", 1, std::bind(&PathToTrajectory::callback, this, _1));
 }
 
 void PathToTrajectory::callback(const autoware_auto_planning_msgs::msg::PathWithLaneId::SharedPtr msg) {
     // particles.csvからPathTrajectoryを生成
-    auto traj = PathTrajectory("particles.csv");
+
+    using namespace my_namespace;
+    std::cout << "callback called" << std::endl;
+    static bool called = false;
+    if (called){
+        return;
+    }
+    std::string path = "/aichallenge/workspace/src/aichallenge_submit/path_to_trajectory/src/particles.csv";
+    auto traj = PathTrajectory(path);
+    std::cout << traj.path_time << std::endl;
+    called = true;
 
     // PathTrajectoryの経路を生成 (dt = 0.01を仮定)
     std::vector<PathPoint> generated_path = traj.generate_path(0.01);
+    if (generated_path.empty()) {
+        std::cout << "Failed to generate path" << std::endl;
+        return;
+    }
+    std::cout << "generated_path.size() = " << generated_path.size() << std::endl;
 
     // ROS 2 の Trajectory メッセージ型に変換
     autoware_auto_planning_msgs::msg::Trajectory trajectory;
-    trajectory.header = msg->header;  // 元のメッセージのヘッダーを使用
+    if (msg == nullptr) {
+        std::cout << "msg is nullptr" << std::endl;
+    }else{
+      trajectory.header = msg->header;  // 元のメッセージのヘッダーを使用
+      std::cout << "trajectory.header.stamp = " << trajectory.header.stamp.sec << std::endl;
+    }
+
+
 
     for (const auto& path_point : generated_path) {
         autoware_auto_planning_msgs::msg::TrajectoryPoint trajectory_point;
@@ -178,7 +226,7 @@ void PathToTrajectory::callback(const autoware_auto_planning_msgs::msg::PathWith
         trajectory_point.pose.position.y = path_point.y;
         trajectory_point.longitudinal_velocity_mps = path_point.vel;
         
-        trajectory.points.push_back(trajectory_point);
+        trajectory.points.emplace_back(std::move(trajectory_point));
     }
 
     // Trajectory メッセージをパブリッシュ
@@ -187,6 +235,17 @@ void PathToTrajectory::callback(const autoware_auto_planning_msgs::msg::PathWith
 
 int main(int argc, char const* argv[]) {
     rclcpp::init(argc, argv);
+    // auto traj =  PathTrajectory("particles.csv");
+    // traj.write_path("path.csv");
+    // std::vector<PathPoint> generated_path = traj.generate_path(0.01);
+    //     for (const auto& path_point : generated_path) {
+    //     autoware_auto_planning_msgs::msg::TrajectoryPoint trajectory_point;
+    //     trajectory_point.pose.position.x = path_point.x;
+    //     trajectory_point.pose.position.y = path_point.y;
+    //     trajectory_point.longitudinal_velocity_mps = path_point.vel;
+        
+    //     trajectory.points.emplace_back(std::move(trajectory_point));
+    // }
     rclcpp::spin(std::make_shared<PathToTrajectory>());
     rclcpp::shutdown();
     return 0;
