adc884bd3ab689a6228376a5a5dabe3d154ea539
Michi-Tsubaki <tsubaki@g.ecc.u-tokyo.ac.jp>
2024-10-10 18:14:16 +0900

    delete spp

--- aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
+++ aichallenge/workspace/src/aichallenge_submit/simple_pure_pursuit/src/simple_pure_pursuit.cpp
@@ -80,22 +80,7 @@ void SimplePurePursuit::onTimer()
   AckermannControlCommand cmd = zeroAckermannControlCommand(get_clock()->now());
   double target_longitudinal_vel = 0.0;  
 
-  if(is_pitstop_) {
-    to_goal = is_pitstop_ ->data;
-  }
 
-  // double cur_vel = sqrt(pow(velocity_->longitudinal_velocity, 2) + pow(velocity_->lateral_velocity, 2)) * 3.6; // mps -> kmh
-  // double left_point = (double) trajectory_->points.size();
-  if (to_goal == 2 && trajectory_->points.size() <= 16){
-    cmd.longitudinal.speed = 0.0;
-    cmd.longitudinal.acceleration = -30.0;
-    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "reached to the pitstop");
-  } else if (to_goal == 3 && (closet_traj_point_idx == trajectory_->points.size() - 1 ||
-    trajectory_->points.size() <= 5)) {
-    cmd.longitudinal.speed = 0.0;
-    cmd.longitudinal.acceleration = -30.0;
-    RCLCPP_INFO_THROTTLE(get_logger(), *get_clock(), 1000 /*ms*/, "reached to the goal");
-  } else {
     // get closest trajectory point from current position
     TrajectoryPoint closet_traj_point = trajectory_->points.at(closet_traj_point_idx);
 
@@ -105,8 +90,7 @@ void SimplePurePursuit::onTimer()
     target_longitudinal_vel = 30.0 * 1000.0 / 3600.0; // km/h -> m/s
     double current_longitudinal_vel = odometry_->twist.twist.linear.x;
     cmd.longitudinal.speed = target_longitudinal_vel;
-    cmd.longitudinal.acceleration =
-      speed_proportional_gain_ * (target_longitudinal_vel - current_longitudinal_vel);
+    cmd.longitudinal.acceleration = speed_proportional_gain_ * (target_longitudinal_vel - current_longitudinal_vel);
 
     // calc lateral control
     //// calc lookahead distance
@@ -143,39 +127,7 @@ void SimplePurePursuit::onTimer()
     cmd.lateral.steering_tire_angle =
       std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance);
 
-    // for (size_t i = 0; i < objects_->data.size(); i += 4) {
-    //   if(i == 12 || i == 16 || i == 20 || i == 24) {
-    //     continue;
-    //   }
-    //   double object_x = objects_->data[i];
-    //   double object_y = objects_->data[i + 1];
-    //   double object_radius = objects_->data[i + 3];
-    //   double object_distance = std::hypot(object_x - odometry_->pose.pose.position.x, object_y - odometry_->pose.pose.position.y);
-    //   double object_angle = std::atan2(object_y - odometry_->pose.pose.position.y, object_x - odometry_->pose.pose.position.x);
-
-    //   double object_radius_sum = object_radius + 4.2;
-    //   //車体から見た物体の角度
-    //   double object_angle_diff = object_angle - tf2::getYaw(odometry_->pose.pose.orientation);
-      
-    //   if (current_steering_ < 1.0){
-    //   //障害物が近づいたとき回避する
-    //     if (object_distance < object_radius_sum && object_distance > object_radius+0.4){
-    //       //物体が前方にあるとき
-    //       if ((object_angle_diff <0.5&&  object_angle_diff > 0.2) ||  (object_angle_diff < -0.2 && object_angle_diff > -0.5)){
-    //         //物体が右側にあるとき
-    //         if (object_angle_diff < 0){
-    //           //左側に避ける
-    //           cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) + 0.36;
-    //         } else {
-    //           //右側に避ける
-    //           cmd.lateral.steering_tire_angle = std::atan2(2.0 * wheel_base_ * std::sin(alpha), lookahead_distance) - 0.36;
-    //         }
-    //       }
-    //     }
-    //   }
-    //   std::cout << "Object x: " << object_x << " Object y: " << object_y << std::endl;
-    // }
-  }
+  
   pub_cmd_->publish(cmd);
   cmd.lateral.steering_tire_angle /=  steering_tire_angle_gain_;
   pub_raw_cmd_->publish(cmd);
