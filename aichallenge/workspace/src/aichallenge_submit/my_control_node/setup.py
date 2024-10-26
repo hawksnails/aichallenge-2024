5153590d74e4b4949e3b08334ae3f92553542d70
Michi-Tsubaki <tsubaki@g.ecc.u-tokyo.ac.jp>
2024-10-26 11:58:12 +0900

    build nodes

---
+++ aichallenge/workspace/src/aichallenge_submit/my_control_node/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'my_control_node'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mtsubaki',
+    maintainer_email='tsubaki@g.ecc.u-tokyo.ac.jp',
+    description='A ROS 2 node that subscribes to control commands and modifies them.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'control_node = my_control_node.control_node:main',
+        ],
+    },
+    package_data={package_name: ['package.xml']},
+
+    include_package_data=True,
+)
