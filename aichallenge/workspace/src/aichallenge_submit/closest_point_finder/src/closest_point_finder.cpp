996431fb280521c1299e16005d284ef3dc4fa4d9
Michi-Tsubaki <tsubaki@g.ecc.u-tokyo.ac.jp>
2024-10-27 15:50:19 +0900

    succeed in publishing /control/debug/closest_point

--- aichallenge/workspace/src/aichallenge_submit/closest_point_finder/src/closest_point_finder.cpp
+++ aichallenge/workspace/src/aichallenge_submit/closest_point_finder/src/closest_point_finder.cpp
@@ -1,36 +1,44 @@
 #include "closest_point_finder/closest_point_finder.hpp"
-
 #include <motion_utils/motion_utils.hpp>
 
-namespace  closest_point_finder
-{
+namespace closest_point_finder {
 
 using motion_utils::findNearestIndex;
 
 ClosestPointFinder::ClosestPointFinder()
-: Node("closest_point_finder")
+: Node("closest_point_finder"),
+  odometry_(nullptr),  // 初期化
+  trajectory_(nullptr) // 初期化
 {
   pub_closest_point_ = create_publisher<TrajectoryPoint>("control/debug/closest_point", 1);
 
   sub_kinematics_ = create_subscription<Odometry>(
-    "input/kinematics", 1, [this](const Odometry::SharedPtr msg) { odometry_ = msg; });
+    "input/kinematics", 1, [this](const Odometry::SharedPtr msg) { 
+      odometry_ = msg; 
+    });
+    
   sub_trajectory_ = create_subscription<Trajectory>(
-    "input/trajectory", 1, [this](const Trajectory::SharedPtr msg) { trajectory_ = msg; });
+    "input/trajectory", 1, [this](const Trajectory::SharedPtr msg) { 
+      trajectory_ = msg; 
+    });
 
   using namespace std::literals::chrono_literals;
-  timer_ =
-    rclcpp::create_timer(this, get_clock(), 30ms, std::bind(&ClosestPointFinder::onTimer, this));
+  timer_ = rclcpp::create_timer(this, get_clock(), 30ms, std::bind(&ClosestPointFinder::onTimer, this));
 }
 
 void ClosestPointFinder::onTimer()
 {
+  // odometry_またはtrajectory_がnullptrか確認
+  if (!odometry_ || !trajectory_) {
+    RCLCPP_WARN(this->get_logger(), "Odometry or trajectory is not available.");
+    return;  // どちらかがnullptrの場合は処理を中断
+  }
+
   // find closest point
-  size_t closet_traj_point_idx =
-    findNearestIndex(trajectory_->points, odometry_->pose.pose.position);
+  size_t closest_traj_point_idx = findNearestIndex(trajectory_->points, odometry_->pose.pose.position);
 
   // publish closest point
-  pub_closest_point_->publish(trajectory_->points.at(closet_traj_point_idx));
-
+  pub_closest_point_->publish(trajectory_->points.at(closest_traj_point_idx));
 }
 
 }  // namespace closest_point_finder
